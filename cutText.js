cd4c43e7889e999ed1d9371ae23512264a598c23
nsiloup <71090230+nsiloup@users.noreply.github.com>
2021-05-03 23:40:32 +0000

    Update cutText.js

--- cutText.js
+++ cutText.js
@@ -1,28 +1,36 @@
-function suite(str, search,  rg) {
-    var result = [];
-    var match;
-    search = new RegExp(search);
-    while (match = search.exec(str)){
-        result.push(match.index)
-    };
-    let finStr = str.substr(result[result.length-1]+1-rg,)//+- 15 pour plus de tolérance
 
-    return finStr;
-}
-//(search, doc)
 let txt = "";
 do{
     txt = prompt("Paste or Enter the original and CLEANED (without break line) text HERE", "");
 }while(txt.length == 0);
 
-let part ;
+let part = "";
 do{
-    part = prompt("/type your search with/ig", "");
+    part = prompt("type your search ", "");
 }while(part.length == 0);
-
-let range;
-do{
-    range = Number(prompt("Enter the range to go back for more tolerance, if you're note sure, put 5", ""));
-}while(!range);
 let look= new RegExp(part, "ig");
-document.write(suite(txt, look, range));
+
+let range = -1;
+do{
+    range = Number(prompt("Enter the range to go back for more tolerance, if you're note sure, put 0", ""));
+}while(range < 0);
+
+let portion = -1;
+do{
+    portion = Number(prompt("Enter the length of the next text please", "")) ;
+}while(portion == -1);
+
+function suite(str, search,  rg, next) {
+    var result = [];
+    var match;
+    search = new RegExp(search);
+    while (match = search.exec(str)){
+        result.push(match.index);
+    };
+
+    let finStr = str.substr(result-rg, next)
+
+    return finStr ;
+}
+
+document.write(suite(txt, look, range, portion));
